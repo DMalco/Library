940002c2b794cfe74ce76d0273e502d00411d806
DMalco <dart_m30@hotmail.com>
2024-04-10 10:29:44 +0100

    v1

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,65 @@
-public class Book {
+/**
+ * A class containing information of Books
+ * @author Dave
+ * @version 14/04/24
+ */
+
+public class Book
+{
+    private String author;
+    private String title;
+    private String ISBN;
+    private boolean onLoan;
+
+
+/**
+ * Constructor to create an object of the Book class
+ * @param aAuthor the author of the book
+ * @param aTitle the title of the book
+ * @param anISBN  the ISBN of the book
+ * @param isOnLoan whether the book is currently out on oan
+ */
+    public Book(String aAuthor, String aTitle, String anISBN, boolean isOnLoan)
+    {
+        this.author = aAuthor;
+        this.title = aTitle;
+        this.ISBN = anISBN;
+        this.onLoan = false;
+    }
+
+    /**Getters
+     *
+     **/
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getISBN()
+    {
+        return ISBN;
+    }
+
+    public boolean isOnLoan()
+    {
+        return onLoan;
+    }
+
+    /**Setters
+     */
+    public void setOnLoan(boolean onLoan)
+    {
+         this.onLoan = onLoan;
+    }
+
+    public String toString()
+    {
+        return "Title: " + this.getTitle() + ", Author: " + this.getAuthor() + this.isOnLoan();
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@
 public class Main {
     public static void main(String[] args)
     {
+        Book b = new Book("Herman Melville", "Moby Dick", "1503280780", true);
+        //Getters
+        b.getAuthor();
+        b.getTitle();
+        b.getISBN();
+        b.isOnLoan();
+
+        //Set onLoan
+        //b.setOnLoan();
 
 
     }
